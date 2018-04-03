f210e8bbccdf4152eeb63254b48fc04a8b9b2bfe
Sebassss <pseba20@gmail.com>
2018-04-03 09:17:44 -0300

    Carpetas
        Class
    
        Clases utiles para el sistema, usuarios, permisos, conexion, otros.

---
+++ class/db.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+define ('DATABASE_NAME' , 'LauncherMSP');
+
+define ('DATABASE_HOST', '10.64.65.200');
+
+define ('DATABASE_USER', 'LauncherMSP_User');
+
+define ('DATABASE_PASS', 'nautilus');
+
+?>
