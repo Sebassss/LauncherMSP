00138bdd75afbc94ba71b7d52371a88d2d9a05d1
ivanneira <ien1983@gmail.com>
2018-04-06 12:52:00 -0300

    con librerías faltantes

--- public/js/user.js
+++ public/js/user.js
@@ -259,6 +259,7 @@ function enviarTicket(){
     $.ajax({
         url: 'http://10.64.65.200:84/otrs/nph-genericinterface.pl/Webservice/bott/Ticket?UserLogin=LauncherMSP&Password=123456',
         dataType: 'json',
+        contentType: 'application/json',
         type: 'POST',
         data: {Article: data2send.Article, Ticket: data2send.Ticket},
         success: function(data){
