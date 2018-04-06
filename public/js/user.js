912659f95d8c1a3275491daba13cf22ded668352
ivanneira <ien1983@gmail.com>
2018-04-06 11:22:12 -0300

    con librerías faltantes

--- public/js/user.js
+++ public/js/user.js
@@ -240,17 +240,17 @@ function enviarTicket(){
     var data2send =
         {"Ticket":
             {
-                'QueueID': '38',
-                'PriorityID': '3',
-                'CustomerUser': $("#customer").select2('data')[0].text,
-                'Title': "Mensaje desde launcher de: " + $("#nombre").val(),
-                'StateID': '1',
-                'Type': $("#tipo").select2('data')[0].text,
-                'Article':
+                "QueueID": "38",
+                "PriorityID": "3",
+                "CustomerUser": $("#customer").select2("data")[0].text,
+                "Title": "Mensaje desde launcher de: " + $("#nombre").val(),
+                "StateID": "1",
+                "Type": $("#tipo").select2("data")[0].text,
+                "Article":
                     {
-                        'ContentType':"text/plain; charset=utf8",
-                        'Subject': 'Reclamo desde Launcher - Teléfono: ' + $("#telefono").val(),
-                        'Body': $("#comentario").val()
+                        "ContentType":"text/plain; charset=utf8",
+                        "Subject": "Reclamo desde Launcher - Teléfono: " + $("#telefono").val(),
+                        "Body": $("#comentario").val()
                     }
             }
         };
